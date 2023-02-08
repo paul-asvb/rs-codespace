b24580276fa9729daa3bf9e450d1df611eeace8d
paul-asvb <4126256+paul-asvb@users.noreply.github.com>
2023-02-08 18:11:47 +0000

    test flattening

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use serde::{Deserialize, Serialize};
+use serde_json;
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug)]
+enum City {
+    Berlin,
+    Paris,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct CityComment {
+    city: City,
+    comment: String,
+}
+
+type TypeOne = Option<Vec<CityComment>>;
+
+type TypeTwo = Option<HashMap<City, String>>;
+
 fn main() {
-    println!("Hello, world!");
+    let type_one = r#"
+        {
+            "Berlin": "bla"
+            "Paris": "bla2"
+        },
+    
+      "#;
+    let type_two = r#"
+      [{
+        "city": "Berlin",
+        "comment": "bla"
+      },{
+        "city": "Paris",
+        "comment": "bla2"
+      }]"#;
+
+    // let v: TypeOne = serde_json::from_str(type_one).unwrap();
+    // println!("{:#?}", v);
+
+    let v: TypeTwo = serde_json::from_str(type_two).unwrap();
+    println!("{:#?}", v);
 }
